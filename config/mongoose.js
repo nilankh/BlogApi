e501b7666721902ca86786e22b9cb621ac93aa9a
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-18 11:52:38 +0530

    working on module created first model admin

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://nilank123:@cluster0.a2xey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://nilank123:nilank123@cluster0.a2xey.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
---
+++ models/admin.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Admin = mongoose.model("Admin", adminSchema);
+module.exports = Admin;
