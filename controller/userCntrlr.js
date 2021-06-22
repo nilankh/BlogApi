6cb0a5cbf1d6452e744dc74ac5fd0aa6b6be6582
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-22 06:54:35 +0530

    controller for user

--- controller/userCntrlr.js
+++ controller/userCntrlr.js
@@ -1,7 +1,7 @@
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
 
-// test controller
+// test controller 
 module.exports.home = function (req, res) {
   return res.json(200, {
     message: "Hello from API's",
@@ -24,7 +24,7 @@ module.exports.create = async function (req, res) {
     // if user not present in the dataBase then creatiing one
     if (!user) {
       User.create(req.body, function (user) {
-        console.log("User", req.body);
+        // console.log("User", req.body);
         return res.status(200).send({
           message: "User created Successfully!!",
         });
@@ -42,10 +42,17 @@ module.exports.create = async function (req, res) {
   }
 };
 
-// Sign in user
+
+// ----------Sign in user
 module.exports.createSession = async (req, res) => {
+
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({
+        message: "Please fill all the required fields !!",
+      });
+    }
   const { email, password } = req.body;
-  const secret_key = "asdfg";
+  const secret_key = "blogapitest";
 
   try {
     let user = await User.findOne({ email });
@@ -59,7 +66,7 @@ module.exports.createSession = async (req, res) => {
     if (user.password == password) {
       return res.status(200).json({
         Message: "Login Successfully!!",
-        token: jwt.sign(user.toJSON(), secret_key, { expiresIn: 1000000 }),
+        token: jwt.sign(user.toJSON(), secret_key, { expiresIn: 10000 }),
       });
     }
   } catch (err) {
