2a6f99ed076e18b456e53b51b676950fb1474dd2
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-22 06:52:22 +0530

    controller for admin

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/user", require("./user"));
-router.use("/admin", require("./admin"));
-router.use("/blog", require("./blog"));
-
-module.exports = router;
