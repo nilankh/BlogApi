eeeb0d39257f5d1d16d5c398598d1758eabea41f
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-06-22 06:47:37 +0530

    routes for admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const adminCntrlr = require("../controllers/adminCntrlr");
 
+// Routes for Admin
+router.post("/login",adminCntrlr.login)
+router.post("/create", adminCntrlr.create);
+router.delete("/delete/:id",adminCntrlr.deleteOne
+);
+
 module.exports = router;
